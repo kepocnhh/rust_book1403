07b1234dc64d9e76ffae12eb744c8b2eede21785
Stanley Wintergreen <kepocnhh@gmail.com>
2023-03-10 20:21:03 +0300

    dependencies/rand/0.8.5

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,13 +1,15 @@
 fn main() {
     println!("Hello Rust book 14/03!");
 
-    let slice = [1, 2, 3];
+    let first = rand::random::<i8>();
+    let slice = [first, 2, 3];
     let it = slices::get_first_or_else(&slice, || &-1);
-    assert_eq!(1, *it);
+    assert_eq!(first, *it);
     println!("first value of {slice:?} is {it}");
 
+    let computes = rand::random::<i8>();
     let slice = [];
-    let it = slices::get_first_or_else(&slice, || &-1);
-    assert_eq!(-1, *it);
+    let it = slices::get_first_or_else(&slice, || &computes);
+    assert_eq!(computes, *it);
     println!("first value of {slice:?} is {it}");
 }
